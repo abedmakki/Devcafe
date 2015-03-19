bc0ab5ca8a06a47e8f0377de107b3c3b62479d35
Mina S. Aziz <minaz922@gmail.com>
2015-03-19 18:12:24 +0200

    include slug field in serialized fields

--- userapp/serializers.py
+++ userapp/serializers.py
@@ -6,4 +6,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'picture', 'birth_date', 'date_joined', 'country', 'address')
+        fields = ('id', 'username', 'email', 'picture', 'birth_date', 'date_joined', 'country', 'address', 'slug')
