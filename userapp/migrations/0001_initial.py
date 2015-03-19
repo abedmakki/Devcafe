95eaf84a0980c2b8a8eee6ee3464586be126e6cd
Mina S. Aziz <minaz922@gmail.com>
2015-03-19 17:20:48 +0200

    implement user model

---
+++ userapp/migrations/0001_initial.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='UserProfile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('picture', models.ImageField(upload_to=b'profile_images', blank=True)),
+                ('birth_date', models.DateField(blank=True)),
+                ('join_date', models.DateField(auto_now_add=True)),
+                ('country', models.CharField(max_length=100)),
+                ('address', models.CharField(max_length=400, null=True)),
+                ('slug', models.SlugField(unique=True)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- userapp/models.py
+++ userapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+
+    # The additional attributes we wish to include.
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    birth_date = models.DateField(blank=True)
+    join_date = models.DateField(auto_now_add=True)
+    country = models.CharField(max_length=100)
+    address = models.CharField(max_length=400, null=True)
+    slug = models.SlugField(unique=True)
+
+    # Override the __unicode__() method to return out something meaningful!
+    def save(self, *args, **kwargs):
+                self.slug = slugify(self.name)
+                super(UserProfile, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.user.username
