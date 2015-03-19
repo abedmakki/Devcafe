cf0cc0efec2025f8cb16f146df7757152f134c6e
Mina S. Aziz <minaz922@gmail.com>
2015-03-19 18:01:13 +0200

    modify User model

--- userapp/models.py
+++ userapp/models.py
@@ -2,14 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class UserProfile(models.Model):
+class User(models.Model):
     # This line is required. Links UserProfile to a User model instance.
     user = models.OneToOneField(User)
 
     # The additional attributes we wish to include.
     picture = models.ImageField(upload_to='profile_images', blank=True)
     birth_date = models.DateField(blank=True)
-    join_date = models.DateField(auto_now_add=True)
     country = models.CharField(max_length=100)
     address = models.CharField(max_length=400, null=True)
     slug = models.SlugField(unique=True)
