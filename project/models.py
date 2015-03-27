a060b22a522c522627ac430437827021178a0d12
abeer <abeer_alwakeel@yahoo.com>
2015-03-27 21:01:12 +0200

    project models implementation

---
+++ project/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Project(models.Model):
+	PM = models.ForeignKey(User)
+	title = models.CharField(max_length=50)
+	description = models.CharField(max_length=500)
+	plan = models.TextField()
+	logo = models.ImageField(upload_to='projects_images', blank=True)
+	slug = models.SlugField(unidue=True)
