17673867006319d76f6bd10543855a77a0492784
Mina S. Aziz <minaz922@gmail.com>
2015-04-04 12:44:35 +0200

    permission system for projects

--- project/views.py
+++ project/views.py
@@ -4,13 +4,16 @@ from django.template.defaultfilters import slugify
 from project.serializers import ProjectSerializer
 from project.models import Project
 from rest_framework import generics
+from project.permissions import IsOwnerOrReadOnly
 
 
 class ProjectList(generics.ListCreateAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
 
 
 class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
