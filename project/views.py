7e8e444c3dc4f27cabd9ce878cada8fc50bcdafb
Mina S. Aziz <minaz922@gmail.com>
2015-04-06 11:27:08 +0200

    some template cleanup

--- project/views.py
+++ project/views.py
@@ -5,15 +5,20 @@ from project.serializers import ProjectSerializer
 from project.models import Project
 from rest_framework import generics
 from project.permissions import IsOwnerOrReadOnly
+from rest_framework import permissions
 
 
 class ProjectList(generics.ListCreateAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (IsOwnerOrReadOnly, permissions.IsAuthenticatedOrReadOnly,)
+
+
+    def perform_create(self, serializer):
+        serializer.save(PM=self.request.user)
 
 
 class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (IsOwnerOrReadOnly, permissions.IsAuthenticatedOrReadOnly)
