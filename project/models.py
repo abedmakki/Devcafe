42ce2248c30740d4ecfac756a83339cd486f9ad1
Mina S. Aziz <minaz922@gmail.com>
2015-04-06 11:21:09 +0200

    generate unique slug for projects

--- project/models.py
+++ project/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
+from time import strftime
 
 
 class Project(models.Model):
@@ -8,12 +9,12 @@ class Project(models.Model):
 	title = models.CharField(max_length=50)
 	description = models.CharField(max_length=500)
 	plan = models.TextField(blank=True)
-	logo = models.ImageField(upload_to='project_images', blank=True)
+	logo = models.ImageField(upload_to='project_images', blank=True, null=True)
 	slug = models.SlugField(unique=True)
 
 	def save(self, *args, **kwargs):
-		self.slug = slugify(self.title)
-		self.PM = request.user
+		self.slug = slugify(self.title + str(self.PM.pk) + strftime("%Y%m%d%S%M%H"))
+		# self.PM = request.user
 		super(Project, self).save(*args, **kwargs)
 
 	def __unicode__(self):
