1047917d2df6c010426d8dee398e3621c06be712
Mina S. Aziz <minaz922@gmail.com>
2015-04-07 20:07:47 +0200

    modify serializers to return each idea with its comments

--- ideas/serializers.py
+++ ideas/serializers.py
@@ -1,12 +1,25 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from ideas.models import Idea
+from ideas.models import Idea, IdeaComment
+from userapp.serializers import UserSerializer
+
+
+class IdeaCommentSerializer(serializers.ModelSerializer):
+
+    owner = UserSerializer(read_only=True)
+
+    class Meta:
+        model = IdeaComment
+        fields = ('id', 'owner', 'timestamp', 'text')
+        read_only_fields = ('owner',)
 
 
 class IdeaSerializer(serializers.ModelSerializer):
     modelslug = serializers.SlugField(read_only=True, source='slug')
+    comments = IdeaCommentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Idea
-        fields = ('id', 'owner', 'title', 'description', 'rating', 'modelslug')
+        fields = ('id', 'owner', 'title', 'description', 'rating', 'modelslug', 'comments')
         read_only_fields = ('owner',)
+
