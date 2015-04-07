cae4d41014903645f374be1b48d8cf8b61bf2f4d
Mina S. Aziz <minaz922@gmail.com>
2015-04-07 16:16:31 +0200

    permission system for ideas

--- ideas/views.py
+++ ideas/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.template.defaultfilters import slugify
 from ideas.serializers import IdeaSerializer
 from ideas.models import Idea
 from rest_framework import generics
+from rest_framework import permissions
+from ideas.permissions import IsOwnerOrReadOnly
 
 
 class IdeaList(generics.ListCreateAPIView):
     queryset = Idea.objects.all()
     serializer_class = IdeaSerializer
+    permission_classes = (IsOwnerOrReadOnly, permissions.IsAuthenticatedOrReadOnly,)
 
     def perform_create(self, serializer):
-        serializer.save(slug=slugify(serializer.data.get('title')))
+        serializer.save(owner=self.request.user)
 
 
 class IdeaDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Idea.objects.all()
     serializer_class = IdeaSerializer
+    permission_classes = (IsOwnerOrReadOnly, permissions.IsAuthenticatedOrReadOnly,)
