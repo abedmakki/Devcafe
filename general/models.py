95aa9b4526b4b81f5d4ed5e043f64456afa2567e
Mina S. Aziz <minaz922@gmail.com>
2015-04-07 19:35:48 +0200

    implement an abstract model for Comments

--- general/models.py
+++ general/models.py
@@ -1,18 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
-from ideas.models import Idea
 
+
+class Comment(models.Model):
+    owner = models.ForeignKey(User)
+    timestamp = models.DateTimeField(auto_now=True)
+    text = models.CharField(max_length=1500)
+
+    class Meta:
+        abstract = True
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField()
-
-
-class IdeaComment(models.Model):
-    owner = models.ForeignKey(User)
-    idea = models.ForeignKey(Idea)
-    timestamp = models.DateTimeField(auto_now=True)
-    text = models.CharField(max_length=1500)
-    # typeflag is set as 0 for Idea Comments, 1 for (Project) Post Comments
-    typeflag = models.IntegerField()
-    # TODO: Add Post foreign key when Post model is implemented
