c98b60693481e2e10d0d23701b3e0b82bea37eca
Mina S. Aziz <minaz922@gmail.com>
2015-04-07 19:35:04 +0200

    remove abstract comment from admin page

--- general/admin.py
+++ general/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from general.models import Tag, IdeaComment
 
 # Register your models here.
-admin.site.register(Tag)
-admin.site.register(IdeaComment)
+# admin.site.register(Tag)
+# admin.site.register(Comment)
