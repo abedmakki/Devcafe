62e437d6ceb8e05332614f771ce2431535d1970b
Mina S. Aziz <minaz922@gmail.com>
2015-04-30 17:55:06 +0200

    view/post ideas JS (UNTESTED)

--- static/javascripts/ideas/ideas.module.js
+++ static/javascripts/ideas/ideas.module.js
@@ -4,15 +4,11 @@
   angular
     .module('devcafe.ideas', [
       'devcafe.ideas.controllers',
-      'devcafe.ideas.directives',
       'devcafe.ideas.services'
     ]);
 
   angular
     .module('devcafe.ideas.controllers', []);
-
-  angular
-    .module('devcafe.ideas.directives', ['ngDialog']);
 
   angular
     .module('devcafe.ideas.services', []);
