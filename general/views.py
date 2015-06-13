2c7ffffba6faf429ee6a3a24b624c8fd369095b2
abeer <abeer_alwakeel@yahoo.com>
2015-06-13 15:55:28 +0200

    Edit Tags

--- general/views.py
+++ general/views.py
@@ -1,10 +1,14 @@
 from general.serializers import TagSerializer
 from general.models import Tag
+from rest_framework.views import APIView
+from rest_framework import permissions
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
 # from rest_framework import permissions
 
 
-class TagList(generics.ListCreateAPIView):
+class TagList(generics.ListAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     # permission_classes = (IsOwnerOrReadOnly, permissions.IsAuthenticatedOrReadOnly,)
@@ -14,3 +18,14 @@ class TagDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     # permission_classes = (IsOwnerOrReadOnly, permissions.IsAuthenticatedOrReadOnly,)
+
+class CreateTag(APIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    def post(self, request):
+        try:
+            tag = Tag.objects.get(name=request.data['name'])
+        except Exception, e:
+            Tag.objects.create(name=request.data['name'])
+            return Response(status=status.HTTP_201_CREATED)
+            # raise e
+        return Response(status=status.HTTP_200_OK)
