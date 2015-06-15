7bebbe6095d93fe34e112d01411f86aa65225719
Mina S. Aziz <minaz922@gmail.com>
2015-06-15 19:26:39 +0200

    fix syntax error when creating a controller alias

--- static/javascripts/devcafe.routes.js
+++ static/javascripts/devcafe.routes.js
@@ -50,11 +50,11 @@
         templateUrl: '/static/templates/marketplace/market-required-info-application.html'
     }).when('/market/post/new', {
         controller: 'MarketplaceController',
-        vm: 'vm',
+        controllerAs: 'vm',
         templateUrl: '/static/templates/marketplace/upload-page-info.html'
     }).when('/market/post/new/upload', {
         controller: 'MarketplaceController',
-        vm: 'vm',
+        controllerAs: 'vm',
         templateUrl: '/static/templates/marketplace/upload-app-files.html'
     }).otherwise('/')
 }
