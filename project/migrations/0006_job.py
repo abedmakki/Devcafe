59f2826d1538bd1190f6e209d4d900eafad0b2e0
abeer <abeer_alwakeel@yahoo.com>
2015-06-17 16:10:05 +0200

    merg migrations conflict

---
+++ project/migrations/0006_job.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('project', '0005_auto_20150617_1453'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Job',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('description', models.CharField(max_length=1000)),
+                ('is_taken', models.BooleanField(default=False)),
+                ('issued_to', models.ForeignKey(related_name='jobs', to=settings.AUTH_USER_MODEL)),
+                ('project', models.ForeignKey(related_name='project_jobs', to='project.Project')),
+            ],
+        ),
+    ]
