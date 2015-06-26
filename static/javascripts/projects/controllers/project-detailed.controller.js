52e2bb3a500938a3786a6cea51b85bb6744cf825
abeer <abeer_alwakeel@yahoo.com>
2015-06-26 18:19:29 +0200

    add progress bar to project detailed page

--- static/javascripts/projects/controllers/project-detailed.controller.js
+++ static/javascripts/projects/controllers/project-detailed.controller.js
@@ -6,16 +6,18 @@
   .module('devcafe.projects.controllers')
   .controller('ProjectDetailedController', ProjectDetailedController);
 
-  ProjectDetailedController.$inject = ['$http', '$location','$scope', '$routeParams', 'Projects'];
+  ProjectDetailedController.$inject = ['$http', '$location', '$scope', '$routeParams', 'Projects'];
 
   function ProjectDetailedController($http, $location, $scope, $routeParams, Projects) {
     var vm = this;
     vm.assignTask = assignTask;
     vm.changeLogo = changeLogo;
+    var progress_ratio = 0;
 
 
     Projects.get($routeParams.id).success(function(data, status, headers, config) {
       $scope.projId = data;
+      progress();
     })
 
     Projects.view_my_tasks($routeParams.id).success(function(data, status, headers, config) {
@@ -44,6 +46,21 @@
 
     }
 
+    function progress(){
+        var tasks_num = $scope.projId.project_tasks.length;
+        var done_num = 0;
+        if (tasks_num === 0){
+          $scope.progress_ratio = 0;
+        }
+        else{
+        for (var i = 0; i < tasks_num; i++){
+          if ($scope.projId.project_tasks[i].is_done === true){
+            done_num++;
+          }
+        }
+        $scope.progress_ratio = Math.floor((done_num/tasks_num)*100);
+      }
+    }
       /**** Change Logo ****/
       $scope.$watch('logo', function () {
           if($scope.logo==null || $scope.logo==undefined || $scope.logo==''){
@@ -56,6 +73,7 @@
       function changeLogo(logo){
           Projects.changeLogo(logo , $scope)
       }
+
 
   }
 
