03ce1ecb416ef073086cae9dd6bdaa5d43a204ea
abedmakki <abedmakki@gmail.com>
2015-06-27 18:46:13 +0200

    fix:make like/unlike without refresh

--- ideas/serializers.py
+++ ideas/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from ideas.models import Idea, IdeaComment, IdeaRating
+from ideas.models import Idea, IdeaComment, IdeaRating , IdeaLike
 from userapp.models import UserProfile
 from userapp.serializers import UserSerializer
 from general.serializers import TagSerializer
@@ -43,11 +43,19 @@ class IdeaSerializer(serializers.ModelSerializer):
     # tags = TagSerializer(many=True, read_only=True)
     tags = serializers.StringRelatedField(many=True)
     owner = serializers.StringRelatedField()
+    is_liked = serializers.SerializerMethodField('get_pm_status')
     # rating = serializers.StringRelatedField(many=True)
     # ratings = IdeaRatingSerializer(many=True, read_only=True)
-
+    def get_pm_status(self, obj):
+        user = self.context['request'].user
+        try:
+            idealiked = IdeaLike.objects.get(owner=user, idea=obj)
+            if idealiked:
+                return True
+        except IdeaLike.DoesNotExist:
+            return False
     class Meta:
         model = Idea
-        fields = ('id', 'owner', 'title', 'description', 'timestamp', 'likes', 'modelslug', 'comments', 'tags', 'avg_rating')
-        read_only_fields = ('owner',)
+        fields = ('id', 'owner', 'title', 'description', 'timestamp', 'likes', 'modelslug', 'comments', 'tags', 'avg_rating','is_liked')
+        read_only_fields = ('owner','is_liked',)
 
