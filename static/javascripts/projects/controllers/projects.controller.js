0ffcd765841a3db8d87d0948bb7ec8670f8cd75e
Mina S. Aziz <minaz922@gmail.com>
2015-06-28 23:44:59 +0200

    fix modal not dismissed when creating a project without an idea

--- static/javascripts/projects/controllers/projects.controller.js
+++ static/javascripts/projects/controllers/projects.controller.js
@@ -9,18 +9,12 @@
     .module('devcafe.projects.controllers')
     .controller('ProjectsController', ProjectsController);
 
-  ProjectsController.$inject = ['$http', '$location','$scope', '$rootScope', '$routeParams', 'Projects'];
+  ProjectsController.$inject = ['$http', '$location','$scope', '$rootScope', '$routeParams', '$timeout', 'Projects'];
 
   /**
   * @namespace ProjectsController
   */
-  // function ProjectsController($scope) {
-  //   var vm = this;
-
-  //   vm.columns = [];
-
-  //   activate();
-  function ProjectsController($http, $location, $scope, $rootScope, $routeParams, Projects) {
+  function ProjectsController($http, $location, $scope, $rootScope, $routeParams, $timeout, Projects) {
             //var userid = Userapp.getAccId();      //we don't need it any more
             var vm = this;
             vm.startProject = startProject;
@@ -45,34 +39,13 @@
             function startProject(){
                 Projects.startProject($scope.ptitle,$scope.pDesc).success(function(data){
                     var createdProjId = data.id;
-                    $location.path('/projects/'+createdProjId+'/');
+                    // $('#StartProj').modal('toggle');
+                    // $('#modal').modal('toggle');
+                    // $('#exampleModalLabel').modal('toggle');
+                    $timeout(function() {                      
+                      $location.path('/projects/'+createdProjId+'/');
+                    }, 500);
                 })
             }
-            // $rootScope.$watch('searchQuery', function() {
-            //   $scope.search = $rootScope.searchQuery;
-            // });
-
-          //   projects.get($routeParams.id).success(function(data, status, headers, config) {            
-          //   $scope.projId = data;                                  
-          // })
-
   }
-  
-  // function applyForJob(jobId){
-  //   console.log(jobId);
-  //   Projects.applyForJob(jobId);
-  // }
-
-
-  // function getProjectId(id) {
-  //   console.log(id);
-  //   Projects.get('projects/' + id + '/').success(function(data, status, headers, config) {
-  //     $scope.projId = data;
-  //     $location.url('projects/' + id);
-  //     console.log(id);
-  //   })
-    
-  // }
-
-  
 })();
