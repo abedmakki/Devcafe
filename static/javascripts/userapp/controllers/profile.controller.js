95136630acdae98c68b41d3897ee988cb9ea8513
Mariam <mariam.ashraf55@yahoo.co[Am>
2015-06-28 11:56:11 +0200

    edit profile working

--- static/javascripts/userapp/controllers/profile.controller.js
+++ static/javascripts/userapp/controllers/profile.controller.js
@@ -8,12 +8,48 @@
     ProfileController.$inject = ['$http', '$location', '$scope', 'Userapp'];
 
 
-      //na2es
+    
 
         function ProfileController($http, $location, $scope, Userapp) {
             //var userid = Userapp.getAccId();      //we don't need it any more
             $http.get('/users/profile/').success(function(data, status, headers, config) {
                 $scope.user = data;
         })
+
+            var vm = this;
+            vm.edit = edit;
+
+
+    
+        function edit(email, username, password, firstname, lastname, birthdate, country, address) {
+            // console.log($scope.user.id);
+            var id = $scope.user.id;
+            
+            // if(email == "")
+            //     email = $scope.user.email;
+            // if(username == "")
+            //     username = $scope.user.username;
+            // // if(password == "")
+            // //     password = $scope.user.password;
+            // if(firstname == "")
+            //     firstname = $scope.user.first_name;
+            // if(lastname == "")
+            //     lastname = $scope.user.last_name;
+            // if(country == "")
+            //     country = $scope.user.country;
+            // if(address == "")
+            //     address = $scope.user.address;
+            // if(birthdate == "")
+            //     birthdate = $scope.user.birth_date;
+            // console.log($scope.user.address);
+            // console.log($scope.user.birthdate);
+            // console.log($scope.user.country);
+            
+
+
+            Userapp.edit(id, username, firstname, lastname, birthdate, password, country,
+         email, address);
+        }
     }
+    
 })();
